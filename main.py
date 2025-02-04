21b8ddf766fa17384797e6d3512cc5b8724ce927
ArthurRamonSouza <arthuramon.souza93@hotmail.com>
2025-02-04 10:18:32 -0300

    test(main): test update

--- main.py
+++ main.py
@@ -1,19 +1,8 @@
 from gui import root
-
-import pytesseract
-from PIL import Image
-
 from invoice import Invoice
 from invoice_reader import InvoiceReader
 
 reader = InvoiceReader()
-invoices: list[Invoice] = reader.get_invoice_images_from_pdf('/home/arthur/Documents/Visual Studio Code/freela/engelmig/boleto_auto/boleto0.pdf')
-print(invoices[0].images)
-print(invoices[0].barcode)
-
-img = invoices[0].images[0]
-text = pytesseract.image_to_string(img, lang="por")
-print("Texto extraído:", text)
-
+reader.get_invoices_from_pdf('/home/arthur/Documents/Visual Studio Code/freela/engelmig/boleto_auto/boleto0.pdf')
 # root.mainloop()
 

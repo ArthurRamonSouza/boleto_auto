6cf1bd67792364eca5857e041b67e1ed97810c3d
ArthurRamonSouza <arthuramon.souza93@hotmail.com>
2025-02-04 07:48:40 -0300

    refact(Invoice): removing some constructor parameters

--- invoice.py
+++ invoice.py
@@ -2,13 +2,13 @@ from PIL import Image
 from datetime import datetime
 
 class Invoice:
-    def __init__(self, barcode: str, type: str, company_name: str = '', creditor_cnpj: str = '', payer_name: str = '', amount: str = '1', due_date: str = '2025/02/03', invoice_number: str = ''):
-        self.barcode: str = barcode
+    def __init__(self, barcode: str, type: str):
         self.type: str = type
+        self.barcode: str = barcode
         self.images: list[Image.Image] = []
-        self.creditor_name: str = company_name
-        self.creditor_cnpj: str = creditor_cnpj
-        self.payer_name: str = payer_name
-        self.amount: float = float(amount)
-        self.due_date: datetime.date = datetime.strptime(due_date, '%Y/%m/%d').date()
-        self.invoice_number: str = invoice_number
+        self.payer_name: str = ''
+        self.amount: float = float('0')
+        self.creditor_name: str = ''
+        self.creditor_cnpj: str = ''
+        self.invoice_number: str = ''
+        self.due_date: datetime.date = None # datetime.strptime(due_date, '%Y/%m/%d').date()

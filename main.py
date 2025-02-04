89670f06d5d84ca4cedc065e50c63b6d981c065d
ArthurRamonSouza <arthuramon.souza93@hotmail.com>
2025-02-04 07:49:07 -0300

    feat(main): Tesseract OCR added in main to tests

--- main.py
+++ main.py
@@ -1,7 +1,19 @@
-from gui import root 
+from gui import root
+
+import pytesseract
+from PIL import Image
+
+from invoice import Invoice
 from invoice_reader import InvoiceReader
 
 reader = InvoiceReader()
-reader.get_pdf_barcode()
+invoices: list[Invoice] = reader.get_invoice_images_from_pdf('/home/arthur/Documents/Visual Studio Code/freela/engelmig/boleto_auto/boleto0.pdf')
+print(invoices[0].images)
+print(invoices[0].barcode)
+
+img = invoices[0].images[0]
+text = pytesseract.image_to_string(img, lang="por")
+print("Texto extraído:", text)
+
 # root.mainloop()
 

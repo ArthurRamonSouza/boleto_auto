46b71b6cffb3b3349bc464d61cba8a5f09a9e5b9
ArthurRamonSouza <arthuramon.souza93@hotmail.com>
2025-02-08 00:57:22 -0300

    feat(invoice): Invoice new methods and regex pattern

--- invoice.py
+++ invoice.py
@@ -1,3 +1,5 @@
+import re
+import io
 from PIL import Image
 from datetime import datetime
 
@@ -9,7 +11,6 @@ class Invoice:
     Attributes:
         type (str): The type of invoice (e.g., 'bank slip', 'utility bill').
         barcode (str): The barcode associated with the invoice.
-        invoice_number (str): The unique invoice number.
         image (Image.Image): The original image of the invoice.
         preprocessed_image (Image.Image): The processed image for OCR purposes.
         payer_name (str): The name of the payer.
@@ -17,10 +18,20 @@ class Invoice:
         amount (float): The total amount of the invoice.
         beneficiary_name (str): The name of the beneficiary.
         beneficiary_number (str): The identification number of the beneficiary (e.g., CNPJ).
-        guantor_name (str): The name of the guarantor (if applicable).
-        guantor_number (str): The identification number of the guarantor (e.g., CPF/CNPJ).
         due_date (datetime.date): The due date of the invoice.
     """
+
+    REGEX_PATTERN: re.Pattern = re.compile(r"""
+        Beneficiário\ ou\ Cedente:\s*(?P<beneficiary_name>.+?)\s*
+        CPF/CNPJ\ do\ Beneficiário\ ou\ Cedente:\s*(?P<beneficiary_number>\d{2}\.\d{3}\.\d{3}/\d{4}[-.]\d{2})\s*
+        Sacado:\s*(?P<payer_name>.+?)\s*
+        CPF/CNPJ\ Sacado:\s*(?P<payer_number>\d{2}\.\d{3}\.\d{3}/\d{4}[-.]\d{2})\s*
+        Vencimento:\s*(?P<due_date>\d{2}/\d{2}/\d{4})\s*
+        Valor\ do\ Documento:\s*R\$\s*(?P<amount>[\d.,]+)\s*
+    """, re.VERBOSE)
+    
+    
+
     def __init__(self, barcode: str, type: str):
         """
         Initializes an Invoice instance with the given barcode and type.
@@ -31,14 +42,44 @@ class Invoice:
         """
         self.type: str = type
         self.barcode: str = barcode
-        self.invoice_number: str = ''
         self.image: Image.Image = None
         self.preprocessed_image: Image.Image = None
-        self.payer_name: str = ''
-        self.payer_number: str = ''
-        self.amount: float = float('0')
-        self.beneficiary_name: str = ''
-        self.beneficiary_number: str = ''
-        self.guantor_name: str = ''
-        self.guantor_number: str = ''
-        self.due_date: datetime.date = None  # Expected to be set later
+        self.payer_name: str = None
+        self.payer_number: str = None
+        self.amount: float = None
+        self.beneficiary_name: str = None
+        self.beneficiary_number: str = None
+        self.due_date: datetime.date = None
+
+    def model_text_to_invoice(self, text_form_model: str) -> None:
+        match_iter = self.REGEX_PATTERN.finditer(text_form_model)
+        match = next(match_iter, None)
+
+        if match:
+            # print(match.groupdict())
+            data: dict = match.groupdict()
+
+            if data.get("amount"):
+                data["amount"] = float(data["amount"].split('$')[0].replace('.', '').replace(',', '.'))
+
+            if data.get("due_date"):
+                data["due_date"] = datetime.strptime(data["due_date"], "%d/%m/%Y").date()
+
+            for key, value in data.items():
+                if value:
+                    print(f'{key}: {value}')
+                    setattr(self, key, value)
+            print('barcode: ', self.barcode)
+
+    def save_invoice(self, download_file_path: str):
+        try:
+            if self.image:
+                image_bytes_io = io.BytesIO()
+                self.image.save(image_bytes_io, format="PNG")
+                image_bytes = image_bytes_io.getvalue()
+
+                with open(download_file_path, 'wb') as file:
+                    file.write(image_bytes)
+
+        except Exception as e:
+            print('Error in method save_invoice:', e)

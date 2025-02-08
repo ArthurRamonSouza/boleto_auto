d44e6aab6011ad3f5e4e2900404f8f502d1b66e6
ArthurRamonSouza <arthuramon.souza93@hotmail.com>
2025-02-08 09:12:26 -0300

    new(db_configuration): script to set the database

---
+++ db_configuration.py
@@ -0,0 +1,19 @@
+import os
+from dotenv import load_dotenv
+from sqlalchemy import create_engine
+from sqlalchemy.orm import declarative_base, sessionmaker
+
+# Configurations
+load_dotenv()
+DATABASE_URL = os.getenv('DB_URL')
+if not DATABASE_URL:
+    raise ValueError("DB_URL não definido no .env")
+
+# Engine is responsible for mannager the db connetion
+engine = create_engine(DATABASE_URL, echo=True)
+
+# SessionLocal provides sessions to interact with the db
+SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+
+# Defines a basefor models
+Base = declarative_base()

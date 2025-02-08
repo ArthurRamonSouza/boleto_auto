dc556431f6c1fb55a70c13cc72325bd21cfb1c43
ArthurRamonSouza <arthuramon.souza93@hotmail.com>
2025-02-08 00:59:14 -0300

    feat&fix(invoice_reader): a lot of fixes and new pipeline to get_invoices_from_pdf

--- invoice_reader.py
+++ invoice_reader.py
@@ -1,18 +1,19 @@
-import re
 import cv2
 import pytesseract
 import numpy as np
 from PIL import Image
 from invoice import Invoice
 from pyzbar.pyzbar import decode
+from model import extract_invoice_data
 from pdf2image import convert_from_path
+
 class InvoiceReader:
 
     def __init__(self):
         pass
 
     # Pega toda a imagem de um boleto no pdf, o codigo de barras e o tipo
-    def get_image_of_invoices_from_pdf(self, pdf_path: str = '', ppi: int = 300) -> list[Invoice]:
+    def __get_image_of_invoices_from_pdf(self, pdf_path: str = '', ppi: int = 300) -> list[Invoice]:
         try:
             pdf_images: list[Image.Image] = convert_from_path(pdf_path, ppi)
             invoice_list: list[Invoice] = []
@@ -37,90 +38,63 @@ class InvoiceReader:
             print(f'Error while decoding the pdf: {e}')
             return None
 
-    # Faz a busca por texto nas imagens do boleto
-    def get_data_from_invoice_image(self, invoice: Invoice) -> None:
-        text = pytesseract.image_to_string(invoice.image, lang='por', config='--psm 12')
-        # print(text)
-        # print("============================================================================================================================================")
+    # Retorna texto presente na imagen do boleto
+    def __get_text_from_invoice_image(self, invoice: Invoice) -> str:
+        try:
+            return pytesseract.image_to_string(invoice.image, lang='por', config='--psm 6')
+        except Exception as e:
+            print('Could not read current image data. Error: ', e)
 
-        invoice_data = {}
-
-        regex_dict = {
-            'bank': r'(\bBradesco\b|\bItaú\b|\bCaixa\b|\bSantander\b|\bBanco do Brasil\b)',
-            # 'barcode': r'(\d{5}\.\d{5} \d{5}\.\d{6} \d \d{14})',
-            'beneficiary_name': r'Beneficiário\s+(.*?)\s+-\s+CNPJ',
-            'beneficiary_number': r'CNPJ\s+([\d\.\-\/]+)',
-            'guantor_name': r'Sacador/Avalista\s+(.*?)\s+-\s+\d{2,3}\.\d{3}\.\d{3}\/\d{4}-\d{2}',
-            'guantor_number': r'Sacador/Avalista\s+.*\s+-\s+([\d\.\-\/]+)',
-            'guantor_number': r'Sacador/Avalista\s+.*\s+-\s+([\d\.\-\/]+)',
-            'payer_name': r'Pagador\s+(.*?)\s+-\s+\d{3}\.\d{3}\.\d{3}-\d{2}',
-            'payer_number': r'Pagador.*?(\d{3}\.\d{3}\.\d{3}-\d{2})',
-            'due_date': r'Data de Vencimento\s+(\d{2}/\d{2}/\d{4})',
-            'amount': r'R\$\s*([\d,]+)',
-            # 'yielding_code': r'Agência / Código do Cedente\s+([\d\/\-]+)',
-            'invoice_number': r'Nosso Número\s*.(\d{2}\/\d{11}-\d{1})'
-        }
-
-        # regex_dict = {
-        #     'bank': r'CAIXA\s*:::\s*(\d{3}-\d)',  # Captura o código do banco (104-0 para Caixa)
-        #     # 'barcode': r'(\d{5}\.\d{5} \d{5}\.\d{6} \d \d{14})',  # Captura código de barras
-        #     'beneficiary_name': r'Cedente\s+([\w\s\-\./]+)',  # Nome do beneficiário
-        #     'beneficiary_number': r'Agência/Código Cedente\s+([\d\/\-]+)',  # Código do Cedente
-        #     'payer_name': r'Sacado\s+([\w\s]+)\s*CPF\/CNPJ:',  # Nome do pagador
-        #     'payer_number': r'CPF\/CNPJ:\s*(\d{3}\.\d{3}\.\d{3}-\d{2})',  # CPF ou CNPJ do pagador
-        #     'due_date': r'Vencimento\s+(\d{2}/\d{2}/\d{4})',  # Data de vencimento
-        #     'amount': r'Valor do Documento\s*R\$\s*([\d,.]+)',  # Valor do boleto
-        #     'invoice_number': r'Nosso Número\s+(\d{8,17}-\d)',  # Nosso Número
-        #     'processing_date': r'Data do Processamento\s+(\d{2}/\d{2}/\d{4})',  # Data do processamento
-        #     # 'document_number': r'Nº do Documento\s+(\d+)',  # Número do documento
-        # }
-
-        for key, pattern in regex_dict.items():
-            match = re.search(pattern, text, re.IGNORECASE)
-            if match:
-                invoice_data[key] = match.group(1)
-                setattr(invoice, key, match.group(1))
-
-        # for key, value in invoice_data.items():
-        #     print(f'{key}: {value}')
-
-    def image_preprocessing(self, invoice: Invoice) -> None:
+    def __image_preprocessing(self, invoice: Invoice) -> None:
         invoice.preprocessed_image = invoice.image.copy()
         image: Image.Image = invoice.image
 
         # 1. Converter PIL para NumPy (necessário para OpenCV)
         image_array = np.array(image)
 
-        # 2. Aumentar a imagem (Interpolation melhora qualidade)
-        scale = 2  # Fator de aumento
+        scale = 2
         height, width = image_array.shape[:2]
         image_array = cv2.resize(image_array, (width * scale, height * scale), interpolation=cv2.INTER_CUBIC)
 
-        # 3. Converter para escala de cinza
         gray = cv2.cvtColor(image_array, cv2.COLOR_BGR2GRAY)
 
-        # 4. Remover ruídos (Desfoque Gaussiano ou Morfologia)
         gray = cv2.GaussianBlur(gray, (5, 5), 0)
 
-        # 5. Binarizar com Otsu
         _, binary = cv2.threshold(gray, 0, 255, cv2.THRESH_BINARY + cv2.THRESH_OTSU)
 
-        # 6. Aplicar morfologia (Fechamento ou Dilatação)
         kernel = np.ones((2,2), np.uint8)
         binary = cv2.morphologyEx(binary, cv2.MORPH_CLOSE, kernel)
 
         # Converter de volta para PIL.Image.Image e salvar em imagens processadas
         preprocessed_image = Image.fromarray(binary)
         invoice.preprocessed_image = preprocessed_image.copy()
-        # preprocessed_image.save("pagina_300dpi.png", "PNG")
+        preprocessed_image.save("pagina_300dpi.png", "PNG")
 
-    # Funcao que sera chamada fora do arquivo e juntara todas as funcoes
     def get_invoices_from_pdf(self, pdf_path: str) -> list[Invoice]:
-        invoices: list[Invoice] = self.get_image_of_invoices_from_pdf(pdf_path)
+        invoices: list[Invoice] = self.__get_image_of_invoices_from_pdf(pdf_path)
 
         for invoice in invoices:
-            self.image_preprocessing(invoice)
-            self.get_data_from_invoice_image(invoice)
-            print(invoice.barcode)
+            self.__image_preprocessing(invoice)
+            invoice_text: str = self.__get_text_from_invoice_image(invoice)
+            model_return: str = extract_invoice_data(invoice_text)
+            print(model_return)
+            invoice.model_text_to_invoice(model_return)
 
+            missing_fields: list = [attr for attr in vars(invoice) if getattr(invoice, attr) is None]
+
+            attempts: int = 0
+            while len(missing_fields) > 0 and attempts < 2:
+                print('Missing values: ', missing_fields)
+                self.__image_preprocessing(invoice)
+                invoice_text: str = self.__get_text_from_invoice_image(invoice)
+                model_return: str = extract_invoice_data(invoice_text)
+                print(model_return)
+                invoice.model_text_to_invoice(model_return)
+
+                missing_fields = [attr for attr in vars(invoice) if getattr(invoice, attr) is None]
+                attempts += 1
+
+            if len(missing_fields) > 0:
+                print(f"Error to create Invoice from: {pdf_path}")
+                invoices.remove(invoice)
         return invoices
